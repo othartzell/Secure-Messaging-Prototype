932b0083d3a885cdf0690c9d4d4309e2a2f491d7
othartzell <ohartzel@uncc.edu>
2025-10-03 13:43:11 -0400

    Updated Test.py to fit assignment instructions
    Changed test cases so Alice and Bob each sign a message. Tested verification using correct and incorrect public keys. Added print statement for public keys.

--- test.py
+++ test.py
@@ -3,25 +3,47 @@
 
 # Testing the functionality of the Digital Signature class
 
+# Imports
 from DigitalSignature import DigitalSignature
+from cryptography.hazmat.primitives import serialization
 
 # Creating two users for testing
 alice = DigitalSignature()
 bob = DigitalSignature()
 
+# Printing Alice's Public Key
+print("Alice's Public Key:")
+print(alice.public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+).decode())
+
+# Printing Bob's Public Key
+print("Bob's Public Key:")
+print(bob.public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+).decode())
+
 # Creating two messages for testing
-message = b"hello world"
-message_two = b"world hello"
+message_a = b"Hello Bob"
+message_b = b"Hello Alice"
 
 # Signing the message as Alice and returning the hex to verify the funciton works
-signature = alice.sign_message(message)
-print("Signature:", signature.hex())
+signature_a = alice.sign_message(message_a)
+print("Alice's Signature:", signature_a.hex())
 
-# Verifying the signature using Alice's public key and the message (Should return 1 if working)
-print("Verify Alice -> Bob:", bob.verify_signature(message, signature, alice.public_key))
+signature_b = bob.sign_message(message_b)
+print("\nBob's Signature:", signature_b.hex())
 
-# Attempting to verify the signature using Bob's public key (Should return 0 if working)
-print("Verify Bob -> Bob:", bob.verify_signature(message, signature, bob.public_key))
+# Verifying the signature of Alice's message using Alice's public key and the message as Bob (Should return 1 if working)
+print("\nVerify Alice -> Bob:", bob.verify_signature(message_a, signature_a, alice.public_key))
 
-# Attempting to verify the signature of an arbitrary message with Alice's public key (Should return 0 if working)
-print("Verify Alice -> Bob:", bob.verify_signature(message_two, signature, alice.public_key))
+# Attempting to verify the signature of Alice's message using Bob's public key as Bob (Should return 0 if working)
+print("Verify Bob -> Bob:", bob.verify_signature(message_a, signature_a, bob.public_key))
+
+# Verifying the signature of Bob's message using Bob's public key and the message as Alice (Should return 1 if working)
+print("Verify Bob -> Alice:", alice.verify_signature(message_b, signature_b, bob.public_key))
+
+# Attempting to verify the signature of Bob's message using Alices's public key as Alice (Should return 0 if working)
+print("Verify Alice -> Alice:", alice.verify_signature(message_b, signature_b, alice.public_key))
